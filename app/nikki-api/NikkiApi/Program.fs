670a846ccd821732c06d8c8857775806aa526afb
tsu-miki <shuta.tsuzuki@uzabase.com>
2025-10-30 21:08:36 +0900

    add tool

--- app/nikki-api/NikkiApi/Program.fs
+++ app/nikki-api/NikkiApi/Program.fs
@@ -1,6 +1,8 @@
 open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Http
 
 [<EntryPoint>]
 let main args =
@@ -8,12 +10,12 @@ let main args =
     let app = builder.Build()
     
     app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
-    
 
-    // app.MapPost("/nikki",  Func<request, Task<IResult>>) {
-    //     return Results.Ok("Hello World!")
-    // }
-    // |> ignore
+    app.MapPost("/nikki", Func<string, Task<IResult>>(fun request ->
+        task {
+            return Results.Ok("Hello World!")
+        }
+    )) |> ignore
     
     app.Run()
     0 // Exit code
