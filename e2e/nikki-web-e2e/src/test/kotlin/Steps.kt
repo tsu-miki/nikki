0cf7fc97bee2b4bd1f6b741cdbc9e7cecfa1ea0e
tsu-miki <shuta.tsuzuki@uzabase.com>
2025-11-14 21:26:44 +0900

    add display title step for healthcheck

--- e2e/nikki-web-e2e/src/test/kotlin/Steps.kt
+++ e2e/nikki-web-e2e/src/test/kotlin/Steps.kt
@@ -1,10 +1,15 @@
 import com.thoughtworks.gauge.Step
 import com.codeborne.selenide.Selenide.*
-import sun.tools.jconsole.inspector.XOpenTypeViewer
+import com.codeborne.selenide.conditions.*
 
 class Steps {
     @Step("ヘルスチェック画面を開く")
     fun nikkiStep() {
        open("/healthCheck")
+    }
+
+    @Step("タイトル<title>が表示されていること")
+    fun titleStep(title: String) {
+        `$`("h1").shouldHave(Text(title))
     }
 }
